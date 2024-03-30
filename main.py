45d6b8ed5744064d4b1aee0e0f8f21db1e94872e
sabrinalily22 <sabrinalily22@gmail.com>
2024-03-30 21:22:13 +0000

    Refactoring #1

--- main.py
+++ main.py
@@ -4,9 +4,9 @@
 #import global
 import time, statistics
 import matplotlib.pyplot as plt
+import LEDControl
 from datetime import datetime, timedelta
 from MCP3008 import MCP3008
-from LEDControl import *  
 from scipy.interpolate import interp1d
 
 import urllib, urllib.parse, threading, random
@@ -15,13 +15,14 @@ import http.client as httplib
 def initialize():
     try:   
         global daily_water_consumption, daily_water_consumption_goal, last_mass, adc, conversion_chart, average_stable_voltage
+
         adc = initialize_adc()
         conversion_chart = initialize_conversion_chart()
         saved_variables = read_variables()
         average_stable_voltage = float(saved_variables[3])
         last_mass = float(saved_variables[2])
         daily_water_consumption, daily_water_consumption_goal = float(saved_variables[4]), 2000  # Example goal        
-        power_on_sequence()
+        LEDControl.power_on_sequence()
     except Exception as e:
         print(f"Error during initialization: {e}")
 
@@ -52,29 +53,67 @@ def process_voltage_readings(voltages, conversion_chart):
 def matlab1(i):
     plt.plot(i)
     plt.show
-    
-    
 
-def initialize_conversion_chart():    
+def initialize_conversion_chart():
+    global spline_model
+   
     # Moved voltages and weights arrays here for clarity
     voltages = [1004, 1000, 994, 983, 981.4, 960.5, 957.2, 953.5, 949, 938, 932, 920, 900, 877, 837, 767, 634, 307.7818182, 166.4457143, 55.45666667]
     weights = [2000, 1500, 1250, 1000, 900, 850, 800, 750, 700, 650, 600, 550, 500, 450, 400, 350, 300, 250, 200, 150]
-    global spline_model
+    
     spline_model = interp1d(voltages, weights, kind='linear', fill_value="extrapolate")
 
 # Function to predict weight from voltage using the spline model
 def convert(voltage): 
-    global spline_model   
+    global spline_model
+
     return spline_model(voltage)
-    
+
 def save_data(current_time, mass, voltage, consumption):
     with open("water_consumption_data", "a") as f:
         data = f"{current_time.strftime('%Y-%m-%d')},{current_time.strftime('%H:%M:%S')},{mass},{voltage},{consumption}\n"
         f.write(data)
+
+def write_consumption_data(current_time, average_stable_mass, average_stable_voltage, daily_water_consumption):
+    global last_recorded_time, stable_voltages, last_mass
+
+    save_data(current_time, "%.1f" % average_stable_mass, "%.1f" % average_stable_voltage, "%1f" % daily_water_consumption)
+    LEDControl.log2()
+
+    if (last_mass - average_stable_mass) > -10:
+        daily_water_consumption += last_mass - average_stable_mass
+
+    last_recorded_time = current_time
+    last_mass = average_stable_mass
+
+def set_stable_voltages(voltages):
+    global stable_voltages
+
+    stable_voltages = convert(voltages[-100:])  # Last 100 readings
+
+def set_stable_averages(stable_voltages):
+    global average_stable_voltage, average_stable_mass, voltages
+
+    average_stable_voltage = voltageharmonic(stable_voltages)
+    average_stable_mass = convert(average_stable_voltage)                
+    voltages = []
     
+    return average_stable_voltage, average_stable_mass, voltages
+
 def get_daily_consumption():
     # Implement daily consumption retrieval here
     pass
+
+def reset_daily_consumption():
+    global daily_water_consumption
+
+    get_daily_consumption()
+    daily_water_consumption = 0
+
+def display_weight_on_LEDs(consumption_goal_percentile):
+    LEDControl.weight_detected()
+    time.sleep(1)
+    LEDControl.weight_percentile(consumption_goal_percentile)
 
 key = 'V1QKO52FOTUWHTZ2'
 def thingspeak_post():
@@ -94,63 +133,57 @@ def thingspeak_post():
     except:
         print ("connection failed")
 
-    
+
 
 def main():
-    initialize() 
+    global voltage, voltages, stable_voltage,  stable_voltages, daily_water_consumption, daily_water_consumption_goal, last_mass, current_time, average_stable_mass, last_recorded_time
+
+    initialize()
+
     last_recorded_time = datetime.now()
     frequency = 0.1  # Example frequency
-    global voltage, voltages, stable_voltage,  stable_voltages, daily_water_consumption, daily_water_consumption_goal, last_mass, current_time
     stable_voltages = []
-    voltages = []   
-    
-  
+    voltages = []
+
     while True:
         current_time = datetime.now()
         voltage = read_voltage()
         #matlab1(voltage)
         #thingspeak_post()
-        if voltage > 70:  # Mass is on the coaster
-            time.sleep(frequency)
-            voltages.append(voltage)
+        mass_is_on_coaster = voltage > 70
+        consumption_goal_percentile = daily_water_consumption / daily_water_consumption_goal
+        time_since_last_recorded = current_time - last_recorded_time
+        
+        print(time_since_last_recorded)
 
-            if len(voltages) == 5:
-                weight_detected()
-                time.sleep(1)
-                weight_percentile(daily_water_consumption / daily_water_consumption_goal)
-
-            #if 300 < len(voltages) <= 301:
-              #  print("hi")
-
-            if len(voltages) > 101:
-                stable_voltage = convert(voltages[-100:])  # Last 100 readings
-                stable_voltages.append(stable_voltage)                               
-
-            if current_time - last_recorded_time > timedelta(minutes=0.5):
-                average_stable_voltage = voltageharmonic(voltages)
-                average_stable_mass = convert(average_stable_voltage)                
-                voltages = []
-
-            if current_time - last_recorded_time > timedelta(minutes=2):
-                save_data(current_time, "%.1f" % average_stable_mass, "%.1f" % average_stable_voltage, "%1f" % daily_water_consumption)
-                log2()
-                
-                if (last_mass - average_stable_mass) > -10:
-                    daily_water_consumption += last_mass - average_stable_mass
-                    
-                last_recorded_time = current_time
-                stable_voltages = []
-                last_mass = average_stable_mass
-
-            if current_time.hour == 0 and current_time.minute == 0:
-                get_daily_consumption()
-                daily_water_consumption = 0
-
-        else:
+        if mass_is_on_coaster == False:
             voltages, stable_voltages = [], []
             last_recorded_time = current_time
-            weight_percentile(daily_water_consumption / daily_water_consumption_goal)
+            LEDControl.weight_percentile(consumption_goal_percentile)
             time.sleep(10)
+            continue
+
+        time.sleep(frequency)
+        voltages.append(voltage)
+
+        if len(voltages) == 5:
+            display_weight_on_LEDs(consumption_goal_percentile)
+
+        #if 300 < len(voltages) <= 301:
+            #  print("hi")
+
+        if len(voltages) > 101:
+            set_stable_voltages(voltages)
+
+        if time_since_last_recorded > timedelta(minutes=0.5):
+            average_stable_voltage, average_stable_mass, voltages = set_stable_averages(stable_voltages)
+
+        if time_since_last_recorded > timedelta(minutes=1):
+            print("1 minute")
+            write_consumption_data(current_time, average_stable_mass, average_stable_voltage, daily_water_consumption)
+
+        if current_time.hour == 0 and current_time.minute == 0:
+            reset_daily_consumption()
 
 if __name__ == "__main__":    
     main()
